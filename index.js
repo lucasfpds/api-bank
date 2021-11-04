930481c9767797ce345614f1cc984de788df728a
Lucas Fernandes <lucasfpd.santos@gmail.com>
2021-11-04 12:43:12 -0300

    Ajustes index

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const app = require("./src/servidor");
 const swaggerUi = require("swagger-ui-express");
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(require("./swagger.json")));
-app.listen(3000);
+app.listen(process.env.PORT);
