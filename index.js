a131f99d903fdfbe694f301c57ab241f0be2514f
Lucas Fernandes <lucasfpd.santos@gmail.com>
2021-11-04 12:58:17 -0300

    Ajsutes swagger

--- index.js
+++
@@ -1,4 +0,0 @@
-const app = require("./src/servidor");
-const swaggerUi = require("swagger-ui-express");
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(require("./swagger.json")));
-app.listen(process.env.PORT);
